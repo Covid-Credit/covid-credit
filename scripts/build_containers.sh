b9bd028fabb13eac9c316bc0331a93524b7b51e1
Jonathan Kim <jkimbo@gmail.com>
2020-03-22 11:20:10 +0000

    Use google secret manager

--- scripts/build_containers.sh
+++ scripts/build_containers.sh
@@ -5,10 +5,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 BASE_DIR="$( dirname "$DIR" )"
 
 # Build be image
-# (
-#   cd "$BASE_DIR/be" || exit
-#   gcloud builds submit --config cloudbuild.yaml .
-# )
+(
+  cd "$BASE_DIR/be" || exit
+  gcloud builds submit --config cloudbuild.yaml .
+)
 
 # Build fe image
 (
