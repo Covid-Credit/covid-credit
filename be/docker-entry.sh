e89271520ed6c5408d5ea56d3b31b186f94115a0
Jonathan Kim <jkimbo@gmail.com>
2020-03-22 10:25:41 +0000

    Add docker-entry script

---
+++ be/docker-entry.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+if [[ $SERVICE_NAME == "be-migrations" ]]; then
+  python manage.py migrate --no-input ; python -m http.server "$PORT"
+else
+  gunicorn --bind ":$PORT" --workers 1 --threads 8 fronted.wsgi
+fi
