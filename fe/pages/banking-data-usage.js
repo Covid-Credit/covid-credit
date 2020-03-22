0d1985ca94a290f420e0c53dc3142a755a634b01
Anthony Mann <anthonyvmann@gmail.com>
2020-03-22 13:09:57 +0000

    Adding bottom margin, sorting header margin on mobile

--- fe/pages/banking-data-usage.js
+++ fe/pages/banking-data-usage.js
@@ -14,7 +14,13 @@ export default function BankingDataUsage() {
   return (
     <>
       <Header />
-      <Box as="form" maxWidth="2xl" margin="auto" mt="16" px="5">
+      <Box
+        as="form"
+        maxWidth="2xl"
+        margin="auto"
+        mt={{ base: "10", sm: "16" }}
+        px="5"
+      >
         <Text
           mb="5"
           fontSize="2xl"
@@ -128,7 +134,7 @@ export default function BankingDataUsage() {
           </Flex>
         </Box>
         <Flex justifyContent="flex-end">
-          <Button variantColor="teal" size="lg">
+          <Button variantColor="teal" size="lg" mb={10}>
             Connect using Credit Kudos
           </Button>
         </Flex>
