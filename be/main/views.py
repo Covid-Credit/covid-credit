fcb85fdab87b91ec7172cb8a83fa277bef867f8a
John Maguire <john.maguire@creditkudos.com>
2020-03-22 16:34:06 +0000

    Remove old create_pdf endpoint

--- be/main/views.py
+++ be/main/views.py
@@ -56,34 +56,3 @@ def complete_credit_kudos(request):
     next_path = request.GET.get("next", f"report/{income_report.reference_code}/view")
     return HttpResponseRedirect(f"{settings.BASE_URL}/{next_path}")
 
-
-def create_pdf(request):
-    template = loader.get_template("pdf.tex")
-    context = {
-        "name": "Sam Pull",
-        "email": "sam@example.com",
-        "dob": "1970/1/1",
-        "utr": "1234567890",
-        "ni": "AB123456C",
-        "address": "123 Fake Street, N1 2AB",
-        "loss_of_income": "partial",
-        "accounts": [
-            {
-                "name": "Sam Pull Current Account",
-                "number": "12345678",
-                "sort_code": "12-34-56",
-            }
-        ],
-        "standard_occupation_code": "6221",  # SOC2020 Hairdresser
-        "company": {"name": "Foo Bar Limited", "number": "11112222",},
-        "logo_path": os.path.join(settings.BASE_DIR, "static/images/creditkudos.png"),
-    }
-    pdf = build_pdf(template.render(context), builder="xelatexmk")
-    storage_client = storage.Client()
-    bucket = storage_client.bucket("covid-credit-dev")
-    blob = bucket.blob("reports/%s.pdf" % uuid.uuid4())
-    blob.upload_from_string(bytes(pdf), content_type="application/pdf")
-
-    return FileResponse(
-        io.BytesIO(bytes(pdf)), as_attachment=False, filename="report.pdf"
-    )
