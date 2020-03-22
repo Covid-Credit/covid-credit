247d6e81e928b05119dffde3e721922931554738
Jonathan Kim <jkimbo@gmail.com>
2020-03-22 17:59:50 +0000

    Save industry data

--- fe/components/Wrapper.js
+++ fe/components/Wrapper.js
@@ -2,11 +2,11 @@ import React from "react";
 import Header from "../components/Header";
 import { Box } from "@chakra-ui/core";
 
-export const Wrapper = ({ children }) => {
+export const Wrapper = ({ children, ...rest }) => {
   return (
     <>
       <Header />
-      <Box as="form" maxWidth="2xl" margin="auto" mt="16" px="5">
+      <Box as="form" maxWidth="2xl" margin="auto" mt="16" px="5" {...rest}>
         {children}
       </Box>
     </>
