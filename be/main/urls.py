f6eaddef1a1c37d8aa9683bf37cf36b68d57f82c
John Maguire <john.maguire@creditkudos.com>
2020-03-22 17:04:07 +0000

    Drop old path mapping

--- be/main/urls.py
+++ be/main/urls.py
@@ -23,7 +23,6 @@ from . import task_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("complete/credit-kudos", views.complete_credit_kudos),
-    path("build_pdf", views.create_pdf),
     path("api/create-report", api_views.create_report),
     path("api/report/credit-kudos-link", api_views.generate_credit_kudos_link,),
     path("_tasks/create-pdf", task_views.create_pdf),
